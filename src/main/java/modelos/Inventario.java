66bc1052fdedadacad5cc2d8edb394bc2d7c5cbd
Hector Rojas <hector.rojass18@gmail.com>
2017-11-21 01:35:40 -0300

    bajando incidencias del servidor con hibernate

--- src/main/java/modelos/Inventario.java
+++ src/main/java/modelos/Inventario.java
@@ -1,62 +1,122 @@
 package modelos;
-
+/**
+ * Clase Inventario.
+ * @author Hector
+ *
+ */
 public class Inventario {
-	private int idInventario;
-	private int manos1;
-	private int manos2;
-	private int pie;
-	private int cabeza;
-	private int pecho;
-	private int accesorio;
-	
-	
-	public int getIdInventario() {
-		return idInventario;
-	}
-	public void setIdInventario(int idInventario) {
-		this.idInventario = idInventario;
-	}
-	public int getManos1() {
-		return manos1;
-	}
-	public void setManos1(int manos1) {
-		this.manos1 = manos1;
-	}
-	public int getManos2() {
-		return manos2;
-	}
-	public void setManos2(int manos2) {
-		this.manos2 = manos2;
-	}
-	public int getPie() {
-		return pie;
-	}
-	public void setPie(int pie) {
-		this.pie = pie;
-	}
-	public int getCabeza() {
-		return cabeza;
-	}
-	public void setCabeza(int cabeza) {
-		this.cabeza = cabeza;
-	}
-	public int getPecho() {
-		return pecho;
-	}
-	public void setPecho(int pecho) {
-		this.pecho = pecho;
-	}
-	public int getAccesorio() {
-		return accesorio;
-	}
-	public void setAccesorio(int accesorio) {
-		this.accesorio = accesorio;
-	}
-	
-	@Override
-	public String toString() {
-		return "Inventario [idInventario=" + idInventario + ", manos1=" + manos1 + ", manos2=" + manos2 + ", pie=" + pie
-				+ ", cabeza=" + cabeza + ", pecho=" + pecho + ", accesorio=" + accesorio + "]";
-	}
-	
+    private int idInventario;
+    private int manos1;
+    private int manos2;
+    private int pie;
+    private int cabeza;
+    private int pecho;
+    private int accesorio;
+/**
+ * Getter de IdInventario.
+ * @return idInventario
+ */
+    public int getIdInventario() {
+        return idInventario;
+    }
+/**
+ * Setter de IdInventario.
+ * @param idInventario del tipo int
+ */
+    public void setIdInventario(final int idInventario) {
+        this.idInventario = idInventario;
+    }
+/**
+ * Getter de Manos1.
+ * @return manos1
+ */
+    public int getManos1() {
+        return manos1;
+    }
+/**
+ * Setter de Manos1.
+ * @param manos1 del tipo int
+ */
+    public void setManos1(final int manos1) {
+        this.manos1 = manos1;
+    }
+/**
+ * Getter de Manos2.
+ * @return manos2
+ */
+    public int getManos2() {
+        return manos2;
+    }
+/**
+ * Setter de Manos2.
+ * @param manos2 del tipo int
+ */
+    public void setManos2(final int manos2) {
+        this.manos2 = manos2;
+    }
+/**
+ * Getter de Pie.
+ * @return pie
+ */
+    public int getPie() {
+        return pie;
+    }
+/**
+ * Setter de Pie.
+ * @param pie del tipo int
+ */
+    public void setPie(final int pie) {
+        this.pie = pie;
+    }
+/**
+ * Getter de Cabeza.
+ * @return cabeza
+ */
+    public int getCabeza() {
+        return cabeza;
+    }
+/**
+ * Setter de Cabeza.
+ * @param cabeza del tipo int
+ */
+    public void setCabeza(final int cabeza) {
+        this.cabeza = cabeza;
+    }
+/**
+ * Getter de Pecho.
+ * @return pecho
+ */
+    public int getPecho() {
+        return pecho;
+    }
+/**
+ * Setter de Pecho.
+ * @param pecho del tipo int
+ */
+    public void setPecho(final int pecho) {
+        this.pecho = pecho;
+    }
+/**
+ * Getter de Accesorio.
+ * @return accesorio
+ */
+    public int getAccesorio() {
+        return accesorio;
+    }
+/**
+ * Setter de Accesorio.
+ * @param accesorio del tipo int
+ */
+    public void setAccesorio(final int accesorio) {
+        this.accesorio = accesorio;
+    }
+/**
+ * Metodo toString.
+ */
+    @Override
+    public String toString() {
+        return "Inventario [idInventario=" + idInventario + ", manos1=" + manos1 + ", manos2=" + manos2 + ", pie="
+    + pie + ", cabeza=" + cabeza + ", pecho=" + pecho + ", accesorio=" + accesorio + "]";
+    }
+
 }
--- src/main/java/modelos/Registro.java
+++ src/main/java/modelos/Registro.java
@@ -1,50 +1,61 @@
 package modelos;
 
+/**
+ * Clase Registro.
+ * @author Hector
+ *
+ */
 public class Registro {
-	private int idPersonaje;
-	private String usuario;
-	private String password;
-	
-	
-	
-	public int getIdPersonaje() {
-		return idPersonaje;
-	}
-
-
-
-	public void setIdPersonaje(int idPersonaje) {
-		this.idPersonaje = idPersonaje;
-	}
-
-
-
-	public String getUsuario() {
-		return usuario;
-	}
-
-
-
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-
-
-
-	public String getPassword() {
-		return password;
-	}
-
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Registro [idPj=" + idPersonaje + ", username=" + usuario + ", password=" + password + "]";
-	}
+    private int idPersonaje;
+    private String usuario;
+    private String password;
+/**
+ * Getter de IdPersonaje.
+ * @return idPersonaje
+ */
+    public int getIdPersonaje() {
+        return idPersonaje;
+    }
+/**
+ * Setter de IdPersonaje.
+ * @param idPersonaje del tipo int
+ */
+    public void setIdPersonaje(final int idPersonaje) {
+        this.idPersonaje = idPersonaje;
+    }
+/**
+ * Getter de Usuario.
+ * @return usuario
+ */
+    public String getUsuario() {
+        return usuario;
+    }
+/**
+ * Setter de Usuario.
+ * @param usuario del tipo string
+ */
+    public void setUsuario(final String usuario) {
+        this.usuario = usuario;
+    }
+/**
+ * Getter de Password.
+ * @return password
+ */
+    public String getPassword() {
+        return password;
+    }
+/**
+ * Setter de Password.
+ * @param password del tipo string
+ */
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+/**
+ * Metodo toString.
+ */
+    @Override
+    public String toString() {
+        return "Registro [idPj=" + idPersonaje + ", username=" + usuario + ", password=" + password + "]";
+    }
 }
