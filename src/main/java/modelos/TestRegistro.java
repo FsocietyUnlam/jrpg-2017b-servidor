1a9107e92665538bb3ead3e7ec94b06698b9ced4
Lucas Magallanes <lucas_magallanes@outlook.com>
2017-11-22 12:11:30 -0300

    Se resolvio merge

--- src/main/java/modelos/TestRegistro.java
+++ src/main/java/modelos/TestRegistro.java
@@ -13,41 +13,49 @@ import org.hibernate.SessionFactory;
 //import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+/**
+ * Clase TestRegistro.
+ * @author Hector
+ *
+ */
 public class TestRegistro {
+/**
+ * Metodo Main.
+ * @param args del tipo string
+ */
+    public static void main(final String[] args) {
 
-	public static void main(String[] args) {
+        System.out.println();
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
 
-		System.out.println();
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();
-		Session session = factory.openSession();
+        try {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Item> cq = cb.createQuery(Item.class);
+            Root<Item> rp = cq.from(Item.class);
+            // cq.select(rp).where(cb.like(rp.get("nombre"), "%s%"));
+            List<Item> lista = session.createQuery(cq).getResultList();
+            for (Item p : lista) {
+                System.out.println(p);
+            }
 
-		try {
-			CriteriaBuilder cb = session.getCriteriaBuilder();
-			CriteriaQuery<Item> cq = cb.createQuery(Item.class);
-			Root<Item> rp = cq.from(Item.class);
-			// cq.select(rp).where(cb.like(rp.get("nombre"), "%s%"));
-			List<Item> lista = session.createQuery(cq).getResultList();
-			for (Item p : lista) {
-				System.out.println(p);
-			}
+            System.out.println("------------------------------");
 
-			System.out.println("------------------------------");
+            CriteriaBuilder cb1 = session.getCriteriaBuilder();
+            CriteriaQuery<Registro> criteriaQuery = cb1.createQuery(Registro.class);
+            Root<Registro> customer = criteriaQuery.from(Registro.class);
+            criteriaQuery.select(customer).where(cb1.equal(customer.get("usuario"), "p1"));
+            List<Registro> lista1 = session.createQuery(criteriaQuery).getResultList();
 
-			CriteriaBuilder cb1 = session.getCriteriaBuilder();
-			CriteriaQuery<Registro> criteriaQuery = cb1.createQuery(Registro.class);
-			Root<Registro> customer = criteriaQuery.from(Registro.class);
-			criteriaQuery.select(customer).where(cb1.equal(customer.get("usuario"), "p1"));
-			List<Registro> lista1 = session.createQuery(criteriaQuery).getResultList();
-			
-			for (Registro p : lista1) {
-				System.out.println("El resultado es" + p.getUsuario());
-			}
-		} catch (HibernateException e) {
-			e.printStackTrace();
-		} finally {
-			session.close();
-			factory.close();
-		}
-	}
+            for (Registro p : lista1) {
+                System.out.println("El resultado es" + p.getUsuario());
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
 
 }
